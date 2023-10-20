398f545b1cafd138e9e20f6298c90fae435b64f8
jeliotj <eliot@eliotsmith.org>
2023-10-20 10:02:12 -0600

    Fixed if it is a draw

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Rock Paper Scissors
 //
 
+var playerSelection = "rock";
+
 function getComputerChoice() {
 
   let choice = Math.floor(Math.random() * 3);
@@ -20,35 +22,63 @@ function playRound(playerSelection, computerSelection) {
 
   let player = playerSelection.toLowerCase();
   let winningChoice;
+  let losingChoice;
   let computer = computerSelection;
   let winLose;
 
   switch (player) {
     case 'rock':
+      
       if (computer === 'scissors') {
         winningChoice = player;
+        losingChoice = computer;
         winLose = 'win';
-      } else {
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      } 
+      else if (computer === 'paper') {
         winningChoice = computer;
+        losingChoice = player
         winLose = 'lose';
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      }
+      else {
+        return "It's a draw! Play again?";
       }
     case 'paper':
       if (computer === 'rock') {
         winningChoice = player;
+        losingChoice = computer;
         winLose = 'win';
-      } else {
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      } 
+      else if (computer === 'scissors') {
         winningChoice = computer;
+        losingChoice = player;
         winLose = 'lose';
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      }
+      else {
+        return "It's a draw! Play again?";
       }
     case 'scissors':
       if (computer === 'paper') {
         winningChoice = player;
+        losingChoice = computer;
         winLose = 'win';
-      } else {
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      } 
+      else if (computer === 'rock') {
         winningChoice = computer;
+        losingChoice = player;
         winLose = 'lose';
+        return `You ${winLose}, ${winningChoice} beats ${losingChoice}!`
+      }
+      else {
+        return "It's a draw! Play again?";
       }
   }
-    return `You ${winLose}, ${winner} beats ${loser}!`
+    
 }
+
+console.log(playRound(playerSelection, getComputerChoice()))
 
